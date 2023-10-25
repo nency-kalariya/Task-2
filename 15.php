153b38f4142e44c9dcfcb7108c0fe23d8d6b2cdc
nency-kalariya <144974985+nency-kalariya@users.noreply.github.com>
2023-10-25 11:08:16 +0530

    Write a PHP script to find the largest of three numbers entered by the user.
    <?php
    $value1 = (integer)readline("Enter value1: ");
    $value2 = (integer)readline("Enter value2: ");
    $value3 = (integer)readline("Enter value3: ");
    echo $value1,$value2,$value3;
    ?>

---
+++ 15.php
@@ -0,0 +1,7 @@
+<?php
+$value1 = (integer)readline("Enter value1: ");
+$value2 = (integer)readline("Enter value2: ");
+$value3 = (integer)readline("Enter value3: ");
+echo $value1,$value2,$value3;
+?>
+
